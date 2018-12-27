bacee750b40f4a3dd0a276a249473d0cbc0d7a20
LukaszSzulc <lukasz.schultz@gmail.com>
2018-12-27 13:53:39 +0100

    Test123

---
+++ AzureDeployTests/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using AzureDeployTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AzureDeployTests.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        private readonly AzureDeployContext context = new AzureDeployContext();
+
+        public TestController()
+        {
+
+        }
+
+        [Route("settings")]
+        [HttpGet]
+        public IHttpActionResult Settings()
+        {
+            return Ok(ConfigurationManager.AppSettings["environment"]);
+        }
+
+        [Route("getAll")]
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            return Ok(context.Persons.ToList());
+        }
+
+        [Route("get/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            var person = context.Persons.Where(x => x.Id == id);
+            return Ok(person);
+        }
+
+        [Route("add")]
+        [HttpPost]
+        public IHttpActionResult Add(Person person)
+        {
+            context.Persons.Add(person);
+            context.SaveChanges();
+            return Created("Add", person.Id);
+        }
+    }
+}
---
+++ AzureDeployTests/Models/AzureDeployContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AzureDeployTests.Models
+{
+    public class AzureDeployContext : DbContext
+    {
+        public AzureDeployContext() : base("database")
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
